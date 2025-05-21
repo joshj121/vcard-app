27d9cf5bf68bcb4a12f297ed1326143fa3dbfe8c
Josh Johnson <jjohnson@bnxc.net>
2025-05-21 13:11:19 -0400

    testing cors again

--- app/Http/Middleware/VerifyShopifyProxy.php
+++ app/Http/Middleware/VerifyShopifyProxy.php
@@ -8,13 +8,17 @@ class VerifyShopifyProxy
 {
     public function handle(Request $request, Closure $next)
     {
+        if ($request->getMethod() === 'OPTIONS') {
+            return $next($request);
+        }
+
         $hmacHeader = $request->header('X-Shopify-Hmac-Sha256');
         $data = $request->isMethod('POST')
-            ? $request->getContent()
-            : $request->getQueryString();
+        ? $request->getContent()
+        : $request->getQueryString();
 
         $calculated = base64_encode(
-            hash_hmac('sha256', $data, config('shopify.secret'), true)
+        hash_hmac('sha256', $data, config('shopify.secret'), true)
         );
 
         if (! hash_equals($hmacHeader, $calculated)) {
